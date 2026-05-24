cf076572fe668aaf4584a5ae989b04f13517b35b
monks-commits <monks@mail.ru>
2026-05-24 17:59:36 +0300

    Update recovery-config.js

--- recovery/recovery-config.js
+++ recovery/recovery-config.js
@@ -1,5 +1,7 @@
 const RECOVERY_SUPABASE_URL = "https://fhusjlkneckbvnrdhbil.supabase.co";
 const RECOVERY_SCAN_ENDPOINT =
   "https://fhusjlkneckbvnrdhbil.supabase.co/functions/v1/recovery-scan";
+const RECOVERY_SUPABASE_KEY =
+  "ВАШ_SUPABASE_ANON_KEY";
 
 const RECOVERY_SCANNER_SECRET = "";
