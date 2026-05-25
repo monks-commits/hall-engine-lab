6440b94bc3162c2fa3aa2f787aeb440508068c8c
monks-commits <monks@mail.ru>
2026-05-25 13:43:39 +0300

    Update recovery-config.js

--- recovery/recovery-config.js
+++ recovery/recovery-config.js
@@ -4,6 +4,5 @@ const RECOVERY_SCAN_ENDPOINT =
 const RECOVERY_SUPABASE_KEY =
   "sb_publishable_nCCfptJOb8Lzy1uAwGBJzA_OJtDneTS";
 
-const RECOVERY_SCANNER_SECRET = "";
 const RECOVERY_SCANNER_SECRET =
-  "your-secret";
+  "SCANNER_SECRET";
