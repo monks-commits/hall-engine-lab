a15c7b2334cffee819261e8612a8764033417cd3
monks-commits <monks@mail.ru>
2026-05-29 11:25:54 +0300

    Create venues.registry.js

---
+++ hall-studio-v1/configs/venues.registry.js
@@ -0,0 +1,8 @@
+window.VA_VENUES = {
+  current: "client-x",
+  items: {
+    "client-x": { label: "Client X", configKey: "VA_CONFIG_CLIENT_X" },
+    "academy": { label: "Academy", configKey: "VA_CONFIG_ACADEMY" },
+    "summer-demo": { label: "Summer Demo", configKey: "VA_CONFIG_SUMMER_DEMO" }
+  }
+};
